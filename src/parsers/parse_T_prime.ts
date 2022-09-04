5804b965cb1f75f0e4051c844dc0e0a43a415503
Bernardo <bernardo.fonpac@gmail.com>
2022-09-04 19:24:44 -0300

    fix on expression (2 + 1) * 3

--- src/parsers/parse_T_prime.ts
+++ src/parsers/parse_T_prime.ts
@@ -2,12 +2,11 @@ import { Lexer, LexerEnum, ParserReturn, operators } from "../Lexer.js"
 import { parse_F, parse_T_prime } from  "./index.js"
 
 export default (lexer: Lexer): ParserReturn => {
-    console.log("PARSE T PRIME", lexer.expression)
+    console.log("PARSE T PRIME")
     const lexerResult = lexer.next();
     if (lexerResult) {
         const [token, operator] = lexerResult
     
-        console.log(token, operator)
         const isMultiplicationOrDivision = token == LexerEnum.OPERATOR && ["*", "/"].includes(operator)
         if (isMultiplicationOrDivision) {
             const F = parse_F(lexer)
